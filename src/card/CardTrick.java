81078295603e57adbdea885c9106c3d0888c0ed8
Muskanpreet kaur <kau15393@sheridancollege.ca>
2024-02-21 13:43:06 -0500

    version11

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -4,29 +4,67 @@
  */
 package card;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
- * A class that fills a magic hand of 7 cards with random Card Objects
- * and then asks the user to pick a card and searches the array of cards
- * for the match to the user's card. To be used as starting code in ICE 1
- * @author srinivsi
+ * A class that models a magic hand of seven cards and allows the user to pick a card
+ * and search for it in the hand.
+ * 
+ * @author [muska]
+ * Student Number: [991715278]
  */
 public class CardTrick {
-    
-    public static void main(String[] args)
-    {
-        Card[] magicHand = new Card[7];
-        
-        for (int i=0; i<magicHand.length; i++)
-        {
-            Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-        }
-        
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
-        // add one luckcard hard code 2,clubs
+
+    private final Card[] magicHand;
+
+    // Constructor to initialize the magic hand with random cards
+    public CardTrick() {
+        magicHand = new Card[7];
+        fillMagicHand();
     }
-    
+
+    // Method to fill the magic hand with random cards
+    private void fillMagicHand() {
+        Random random = new Random();
+        for (int i = 0; i < magicHand.length; i++) {
+            Card card = new Card();
+            card.setValue(random.nextInt(13) + 1); // Random value between 1 and 13
+            card.setSuit(Card.SUITS[random.nextInt(4)]); // Random suit from the suits array
+            magicHand[i] = card;
+        }
+    }
+
+    // Method to ask the user to pick a card and search for it in the magic hand
+    public void pickAndSearch() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the value of your card (1-13): ");
+        int value = scanner.nextInt();
+        System.out.print("Enter the suit of your card (hearts, diamonds, clubs, spades): ");
+        String suit = scanner.next().toLowerCase();
+        
+        Card userCard = new Card();
+        userCard.setValue(value);
+        userCard.setSuit(suit);
+
+        boolean found = false;
+        for (Card card : magicHand) {
+            if (card.equals(userCard)) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            System.out.println("Congratulations! Your card is in the magic hand.");
+        } else {
+            System.out.println("Sorry, your card is not in the magic hand.");
+        }
+    }
+
+    // Main method to test the CardTrick class
+    public static void main(String[] args) {
+        CardTrick cardTrick = new CardTrick();
+        cardTrick.pickAndSearch();
+    }
 }
